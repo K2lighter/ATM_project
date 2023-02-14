3ed26b8b4f80e475264ff3d0bf1e054167369830
k2lighter <k2lighter@gmail.com>
2023-02-14 13:56:00 +0300

    added new functional in atm work

--- atm.py
+++ atm.py
@@ -6,6 +6,7 @@ class ATM:
     def atm_logic(self):
         Sql_Atm.create_table()
         # Sql_Atm.insert_users((1234, 1111, 10000))
+        # Sql_Atm.insert_users((2345, 2222, 10000))
         number_card = input("Введите пожалуйста номер карты: ")
 
         while True:
--- sql_query.py
+++ sql_query.py
@@ -1,6 +1,12 @@
 import sqlite3
 
 
+class Text:
+    text_get_money = "Введите пожалуйста сумму которую желаете снять: \n"
+    text_transfer_money = "Введите пожалуйста сумму которую желаете перевести: \n"
+    text_deposition_money = "Введите пожалуйста сумму которую желаете внести: \n"
+
+
 class Sql_Atm:
 
     @staticmethod
@@ -74,10 +80,10 @@ class Sql_Atm:
             print(f"Баланс вашей карты: {balance_card}")
 
     @staticmethod
-    def withdraw_money(number_card):
+    def withdraw_money(number_card, text_input):
         """Снятие денежных средств с карты"""
 
-        amount = input("Введите пожалуйста сумму которую желаете снять: ")
+        amount = input(text_input)
         with sqlite3.connect('atm.db') as db:
             cur = db.cursor()
             cur.execute(f"""SELECT Balance FROM Users_data WHERE Number_card = {number_card}""")
@@ -98,10 +104,10 @@ class Sql_Atm:
                 return False
 
     @staticmethod
-    def deposition_money(number_card):
+    def deposition_money(number_card, text_input):
         """Внести денежные средства на баланс"""
 
-        amount = input("Введите пожалуйста сумму которую желаете внести: ")
+        amount = input(text_input)
         with sqlite3.connect('atm.db') as db:
             try:
                 cur = db.cursor()
@@ -109,6 +115,59 @@ class Sql_Atm:
                     f"""UPDATE Users_data SET Balance = Balance + {amount} WHERE Number_card = {number_card};""")
                 db.commit()
                 Sql_Atm.info_balance(number_card)
+            except:
+                print("Попытка выполнить некорректное действие")
+                return False
+
+    @staticmethod
+    def transfer_money(number_card):
+        """Перевести денежные средства"""
+
+        amount = input(Text.text_transfer_money)
+        with sqlite3.connect('atm.db') as db:
+            cur = db.cursor()
+            cur.execute(f"""SELECT Balance FROM Users_data WHERE Number_card = {number_card}""")
+            result_info_balance = cur.fetchone()
+            balance_card = result_info_balance[0]
+            try:
+                if int(amount) > balance_card:
+                    print("На вашей карте недостаточно денежных средств")
+                    return False
+                else:
+                    cur.execute(
+                        f"""UPDATE Users_data SET Balance =
+                         Balance - {amount} WHERE Number_card = {number_card};""")
+                    db.commit()
+                    card_recipient = input(f"Введите номер карты получателя \n")
+                    confirm = input("Для подтверждения операции введите - yes \n")
+                    if confirm == 'yes':
+                        try:
+                            with sqlite3.connect("atm.db") as db:
+                                cur = db.cursor()
+                                cur.execute(f"""SELECT Number_card
+                                 FROM Users_data WHERE Number_card = {card_recipient}""")
+                                result_card = cur.fetchone()
+                                if result_card is None:
+                                    print("Введен неизвестный номер карты")
+                                    return False
+                                else:
+                                    db = sqlite3.connect("atm.db")
+                                    cur = db.cursor()
+                                    cur.execute(
+                                        f"""UPDATE Users_data SET Balance = Balance + {amount}
+                                         WHERE Number_card = {card_recipient};""")
+                                    db.commit()
+                                    print(f"Перевод денежных средств в размере:"
+                                          f" {amount} тубриков на карту №{card_recipient} успешно выполнен")
+                                    Sql_Atm.info_balance(number_card)
+                                    return True
+                        except:
+                            print("Попытка выполнить некорректное действие")
+                            return False
+                    else:
+                        print("Вы не подтвердили операцию, возвращаемся в главное меню")
+                        return False
+
             except:
                 print("Попытка выполнить некорректное действие")
                 return False
@@ -122,21 +181,25 @@ class Sql_Atm:
                               "1. Узнать баланс \n"
                               "2. Снять денежные средства \n"
                               "3. Внести денежные средства \n"
-                              "4. Завершить работу \n")
+                              "4. Завершить работу \n"
+                              "5. Перевести денежные средства \n")
 
             match operation:
                 case "1":
                     Sql_Atm.info_balance(number_card)
 
                 case "2":
-                    Sql_Atm.withdraw_money(number_card)
+                    Sql_Atm.withdraw_money(number_card, Text.text_get_money)
 
                 case "3":
-                    Sql_Atm.deposition_money(number_card)
+                    Sql_Atm.deposition_money(number_card, Text.text_deposition_money)
 
                 case "4":
                     print("Спасибо за ваш визит, всего доброго")
                     return False
+
+                case "5":
+                    Sql_Atm.transfer_money(number_card)
 
                 case _:
                     print("Данная операция недоступна, попробуйте другую операцию")
